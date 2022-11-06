614531d958b933fddfeb3c74b6c7776225dc7e8b
Noveboi <cptgiorgos03@gmail.com>
2022-11-06 19:19:33 +0200

    movement system polished, added bullet firing for player

--- Space_Game/Game.Designer.cs
+++ Space_Game/Game.Designer.cs
@@ -30,6 +30,7 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Game));
             this.p = new System.Windows.Forms.PictureBox();
+            this.log = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.p)).BeginInit();
             this.SuspendLayout();
             // 
@@ -43,24 +44,44 @@
             this.p.TabIndex = 0;
             this.p.TabStop = false;
             // 
+            // log
+            // 
+            this.log.BackColor = System.Drawing.Color.GhostWhite;
+            this.log.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.log.Enabled = false;
+            this.log.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.log.ForeColor = System.Drawing.SystemColors.Menu;
+            this.log.Location = new System.Drawing.Point(12, 12);
+            this.log.Multiline = true;
+            this.log.Name = "log";
+            this.log.ReadOnly = true;
+            this.log.Size = new System.Drawing.Size(200, 300);
+            this.log.TabIndex = 0;
+            this.log.TabStop = false;
+            // 
             // Game
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkSlateBlue;
             this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.log);
             this.Controls.Add(this.p);
             this.Name = "Game";
             this.Text = "Game";
             this.Load += new System.EventHandler(this.Game_Load);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Game_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Game_KeyPress);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Game_KeyUp);
             ((System.ComponentModel.ISupportInitialize)(this.p)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.PictureBox p;
+        private System.Windows.Forms.TextBox log;
     }
 }
--- Space_Game/Game.cs
+++ Space_Game/Game.cs
@@ -12,18 +12,57 @@ namespace Space_Game
 {
     public partial class Game : Form
     {
+        //player attributes
         private int playerSpeed;
         private Size playerSize;
+
+        //bullet variables
+        private List<PictureBox> bullets = new List<PictureBox>();
+        private Timer bulletTimer = new Timer { Interval = 25 };
+
+        //movement variables
+        private Timer movementTimer = new Timer { Interval = 25 };
+        private bool mu, md, ml, mr;
+        private Keys kdk = Keys.None;
+
+        //paths
+        private string spritePath = "../../Sprites/";
 
         public Game()
         {
             InitializeComponent();
+            movementTimer.Tick += MovementTimer_Tick;
+            bulletTimer.Tick += BulletTimer_Tick;
+        }
+
+        private void BulletTimer_Tick(object sender, EventArgs e)
+        {
+            foreach (var bullet in bullets.ToList())
+            {
+                TranslateBullet(bullet);
+
+                //Despawn bullet if it goes off screen
+                if(bullet.Location.Y <= -bullet.Height)
+                {
+                    Controls.Remove(bullet);
+                    bullets.Remove(bullet);
+                    log.AppendText("bullet removed!"+Environment.NewLine);
+                }
+
+                //Despawn bullet if it hits enemy
+            }
+        }
+
+        private void MovementTimer_Tick(object sender, EventArgs e)
+        {
+            PlayerMove();
         }
 
         private void Game_Load(object sender, EventArgs e)
         {
-            playerSpeed = 35;
+            playerSpeed = 20;
             playerSize = p.Size;
+            Focus();
         }
 
         /// <summary>
@@ -31,30 +70,121 @@ namespace Space_Game
         /// </summary>
         private void Game_KeyDown(object sender, KeyEventArgs e)
         {
+            if (kdk != Keys.None && kdk == e.KeyCode) return;
+            kdk = e.KeyCode;
             switch (e.KeyCode)
             {
                 //Move up
                 case Keys.W:
-                    if (p.Location.Y > Height/2) 
-                    { p.Location = new Point(p.Location.X, p.Location.Y - playerSpeed); }
+                    mu = true;
                     break;
                 //Move left
                 case Keys.A:
-                    if (p.Location.X > 10) 
-                    { p.Location = new Point(p.Location.X - playerSpeed, p.Location.Y); }
+                    ml = true;
                     break;
                 //Move down
                 case Keys.S:
-                    if (p.Location.Y < Height - (playerSize.Height + playerSpeed + 20)) 
-                    { p.Location = new Point(p.Location.X, p.Location.Y + playerSpeed); }
+                    md = true;
                     break;
                 //Move right
                 case Keys.D:
-                    if (p.Location.X < Width - (playerSize.Width + playerSpeed + 10) ) 
-                    { p.Location = new Point(p.Location.X + playerSpeed, p.Location.Y); }
+                    mr = true;
+                    break;
+                case Keys.Space:
+                    SpawnBullet(p.Location);
+                    log.AppendText($"Bullet Fired at {p.Location}{Environment.NewLine}");
+                    log.AppendText($"Total Bullets Alive: {bullets.Count}{Environment.NewLine}");
+                    bulletTimer.Start();
+                    break;
+            }
+            PlayerMove();
+            movementTimer.Start();
+        }
+        private void Game_KeyUp(object sender, KeyEventArgs e)
+        {
+            kdk = Keys.None;
+            if (e.KeyCode == Keys.Space) return;
+
+            switch (e.KeyCode)
+            {
+                //Move up
+                case Keys.W:
+                    mu = false;
+                    break;
+                //Move left
+                case Keys.A:
+                    ml = false;
+                    break;
+                //Move down
+                case Keys.S:
+                    md = false;
+                    break;
+                //Move right
+                case Keys.D:
+                    mr = false;
                     break;
             }
 
+            if (!(mu || ml || md || mr))
+            {
+                movementTimer.Stop();
+            }
+
+        }
+
+        private void Game_KeyPress(object sender, KeyPressEventArgs e)
+        { 
+           
+        }
+
+        //Player movement system inspired by: https://stackoverflow.com/a/29957353
+        #region Player Movement Methods
+
+        private int yReduction = 10;
+
+        void MoveUp()
+        {
+            if (p.Location.Y > Height / 2 - 100)
+            { p.Location = new Point(p.Location.X, p.Location.Y - (playerSpeed - yReduction)); }
+        }
+        void MoveLeft()
+        {
+            if (p.Location.X > 10)
+            { p.Location = new Point(p.Location.X - playerSpeed, p.Location.Y); }
+        }
+        void MoveDown()
+        {
+            if (p.Location.Y < Height - (playerSize.Height + playerSpeed + 20))
+            { p.Location = new Point(p.Location.X, p.Location.Y + (playerSpeed - yReduction)); }
+        }
+        void MoveRight()
+        {
+            if (p.Location.X < Width - (playerSize.Width + playerSpeed + 10))
+            { p.Location = new Point(p.Location.X + playerSpeed, p.Location.Y); }
+        }
+        void PlayerMove()
+        {
+            if (mu) MoveUp();
+            if (ml) MoveLeft();
+            if (md) MoveDown();
+            if (mr) MoveRight();
+        }
+        #endregion
+
+        void SpawnBullet(Point currentPlayerLoc)
+        {
+            PictureBox bullet = new PictureBox();
+            bullet.Image = Image.FromFile($"{spritePath}bullet.png");
+            bullet.SizeMode = PictureBoxSizeMode.StretchImage;
+            bullet.Size = new Size(10, 60);
+            bullet.Location = new Point(currentPlayerLoc.X + playerSize.Width / 2 - 5, currentPlayerLoc.Y - playerSize.Height / 2 + 6);
+            Controls.Add(bullet);
+            bullets.Add(bullet);
+        }
+
+        void TranslateBullet(PictureBox bullet)
+        {
+            bullet.Location = new Point(bullet.Location.X, bullet.Location.Y - 14);
         }
     }
 }
